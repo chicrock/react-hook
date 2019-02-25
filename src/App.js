0fc50bad9efe89f3690e98e9a8cba73ff13125c1
chicrock <jungsungrak@gmail.com>
2019-02-25 09:24:47 +0900

    useFetch with useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const useInput = defaultValue => {
   const [value, setValue] = useState(defaultValue);
@@ -14,14 +15,44 @@ const useInput = defaultValue => {
   return { value, onChange };
 };
 
+const useFetch = url => {
+  const [payload, setPayload] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const callUrl = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setPayload(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    callUrl();
+  }, []);
+
+  return { payload, loading, error };
+};
+
 const App = () => {
   const nameInput = useInput("");
+  const { payload, loading, error } = useFetch("https://aws.random.cat/meow");
 
   return (
     <div>
       <h1>Use Hooks</h1>
       <br />
       <input {...nameInput} placeholder="What's your name" />
+      <br />
+      {loading && <span>Loading cat...</span>}
+      {!loading && error && <span>Error: {error}</span>}
+      {!loading && !error && payload && (
+        <img alt="cats" src={payload.file} width={150} />
+      )}
     </div>
   );
   /*
